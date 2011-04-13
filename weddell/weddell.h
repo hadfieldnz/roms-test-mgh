a96263dd56f8c6b91a73844d3086972723f72392
arango <arango@f091316a-d328-0410-a40a-876eff57d070>
2011-04-13 22:05:05 +0000

    src:ticket:498
    git-svn-id: https://www.myroms.org/svn/src/test@548 f091316a-d328-0410-a40a-876eff57d070

---
+++ weddell/weddell.h
@@ -0,0 +1,40 @@
+/*
+** svn $Id$
+*******************************************************************************
+** Copyright (c) 2002-2011 The ROMS/TOMS Group                               **
+**   Licensed under a MIT/X style license                                    **
+**   See License_ROMS.txt                                                    **
+*******************************************************************************
+**
+** Options for Idealized Weddell Sea Application: Tides and Ice Shelf Test.
+**
+** Application flag:   WEDDELL
+** Input script:       ocean_wedell.in
+*/
+
+#define UV_ADV
+#define DJ_GRADPS
+#define UV_COR
+#define UV_QDRAG
+#define SOLVE3D
+#define SALINITY
+#define NONLIN_EOS
+#define CURVGRID
+#define SPLINES
+
+#define NS_PERIODIC
+#define EAST_FSCHAPMAN
+#define EAST_M2FLATHER
+#define WEST_FSCHAPMAN
+#define WEST_M2FLATHER
+
+#define ANA_GRID
+#define ANA_INITIAL
+#define ANA_FSOBC
+#define ANA_M2OBC
+#define ANA_SMFLUX
+#define ANA_STFLUX
+#define ANA_SSFLUX
+#define ANA_SRFLUX
+#define ANA_BSFLUX
+#define ANA_BTFLUX
