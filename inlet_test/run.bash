e6abc2b1563c7006e129adee8d308b976895f963
Mark Hadfield <mark.hadfield@niwa.co.nz>
2018-02-14 11:10:58 +1300

    Polishing the build process

--- inlet_test/run.bash
+++ inlet_test/run.bash
@@ -3,5 +3,8 @@
 # MGH 2011-05-18
 #   A script to run the INLET_TEST case and save the output. Before running
 #   this script, build the executable with build.bash
+# MGH 2018-02-14
+#   The executable should now be built with "roms_build -m", using environment
+#   variables set in .build_env. The executable name is now "ocean".
 
-mpirun -np 2 ./oceanM coupling_inlet_test.in | tee inlet_test.log
+mpirun -np 2 ./ocean coupling_inlet_test.in | tee inlet_test.log
