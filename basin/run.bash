91d414cd865baeef20526a3ebd395bc7bcdc17d5
Mark Hadfield <mark.hadfield@niwa.co.nz>
2018-10-04 16:19:28 +1300

    Updated the BASIN and KELVIN cases.

--- basin/run.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# MGH 2011-06-28
-#   A script to run the BASIN case and save the output. Before running
-#   this script, build the executable with build.bash
-
-mpirun -np 4 ./oceanM ocean_basin.in | tee basin.log
--- kelvin/run.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# MGH 2018-02-20
-#   A script to run the KELVIN case and save the output. Before running
-#   this script, build the executable with roms_build
-
-./ocean < ocean_kelvin.in | tee ocean_kelvin.log
